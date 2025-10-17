90d83c558a4e1c143344d3bb31d195cd39dac3a8
sebastianOrozco7 <sebastian.orozco@cun.edu.co>
2025-10-16 22:26:18 -0500

    yo vere wilson

--- ZonaRival/Models/ViewModels/EquipoViewModel.cs
+++ ZonaRival/Models/ViewModels/EquipoViewModel.cs
@@ -3,8 +3,10 @@
     public class EquipoViewModel
     {
         public Equipo equipoViewModel { get; set; } = new();
+        public Partido partidoViewModel { get; set; } = new();
         public List<Equipo> ListaEquipos { get; set; } = new();
         public List<Cancha> ListaCanchas {  get; set; } = new();
+        public List<Partido> ListaPartidos {  get; set; } = new();
 
     }
 }
